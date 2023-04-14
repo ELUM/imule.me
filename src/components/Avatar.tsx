707bfb51eb12a00a066c1c480ceaf57da6d608d7
Mule <1533238648@qq.com>
2023-04-14 17:16:58 +0800

    avatar animation

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,12 @@
+
+const Avatar = () => {
+    return (
+        <div className="Avatar">
+            <img src="https://avatars.githubusercontent.com/u/40533546?v=4" alt="" />
+            <h1>Mule</h1>
+            <p>Welcome my website!<span className="avatar-cursor">|</span></p>
+        </div>
+    )
+}
+
+export default Avatar;
